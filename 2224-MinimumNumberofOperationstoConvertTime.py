c4ac6c53a8c7ae76fb50f3f3f5b61251c418200a
Eilon Tal <eilontal11@gmail.com>
2024-09-07 15:14:23 +0300

    add 2224 draft

---
+++ 2224-MinimumNumberofOperationstoConvertTime.py
@@ -0,0 +1,18 @@
+class Solution:
+    def addTime(self, time: str, added: int) -> str:
+        pass
+    
+    def isTimeSmaller(self, time: str, time_to_compare_to: str) -> bool:
+        pass
+
+    def convertTime(self, current: str, correct: str) -> int:
+        addition_possibilities_sorted: List[int] = [60, 15, 5, 1]
+        minimum_number_of_operations: int = 0
+        while current < correct:
+            for addition_possibility in addition_possibilities_sorted:
+                time_after_operation: str = addTime(current, addition_possibility)
+                if isTimeSmaller(time_after_operation, correct):
+                    current = time_after_operation
+                    break
+            minimum_number_of_operations += 1
+        return minimum_number_of_operations 
